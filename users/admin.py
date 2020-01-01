3a143abe9aa416eb65c6ec0905fb695ac6a8e1ff
dizhu <dizhu210@gmail.com>
2020-01-01 15:33:48 -0500

    email verification is mandatory

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import UserProfile
-
+# from recipes.models import Recipe
 # Register your models here.
 admin.site.register(UserProfile)
+# admin.site.register(Recipe)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class UserProfile(AbstractUser):
     GENDER_CHOICES = (
         ('male', 'Male'),
