134dc2f6e69526c4e59a73019673d4042325af1a
dizhu <dizhu210@gmail.com>
2020-01-01 21:06:16 -0500

    social account works

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile
+from django.contrib.auth.decorators import login_required
 # from recipes.models import Recipe
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.login = login_required(admin.site.login)
 # admin.site.register(Recipe)
