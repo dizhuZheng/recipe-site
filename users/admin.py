e1e97e5c5c6a71b845eaf7bf3ad185954fc216ca
dizhu <dizhu210@gmail.com>
2020-01-01 21:33:55 -0500

    social app login works

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import UserProfile
 from django.contrib.auth.decorators import login_required
 # from recipes.models import Recipe
+
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.login = login_required(admin.site.login)
