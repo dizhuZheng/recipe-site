6f39ae731eeef237eb9f6912fc806faf2528c8a5
dizhu <dizhu210@gmail.com>
2020-06-12 20:32:08 -0400

    hightlight

--- users/es_call.py
+++ users/es_call.py
@@ -4,7 +4,9 @@ from elasticsearch_dsl import Search, Q
 def esearch(username="", gender="", address="", email="", photo=""):
     client = Elasticsearch()
     q = Q("bool", should=[Q("match", username=username), Q("match", photo=photo), Q("match", address=address), Q("match", email=email), Q("match", gender=gender)], minimum_should_match=1)
-    s = Search(using=client, index="users").query(q)[0:20]
+    s = Search(using=client, index="users").query(q)
+    s = s.highlight_options(order='score', require_field_match='false', fields={ "*": { "pre_tags": [ "<font color='red'>" ], "post_tags": [ "</font>" ]}})
+    s = s.highlight('username', fragment_size=50)
     response = s.execute()
     search = get_results(response)
     return search
@@ -13,6 +15,8 @@ def esearch(username="", gender="", address="", email="", photo=""):
 def get_results(response):
     results = []
     for hit in response:
+        for fragment in hit.meta.highlight.username:
+            print(fragment)
         result_tuple = (hit.username, hit.gender, hit.email, hit.address, hit.photo)
         results.append(result_tuple)
     return results
